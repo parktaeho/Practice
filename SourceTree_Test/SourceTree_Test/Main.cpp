84c1f00b481d71e21fbc65aa45a242d353c543cc
박태호 <marron89@navcours.com>
2017-06-20 11:08:59 +0900

    <코드> * 수정

--- SourceTree_Test/SourceTree_Test/Main.cpp
+++ SourceTree_Test/SourceTree_Test/Main.cpp
@@ -3,7 +3,5 @@
 void main(void)
 {
 	printf("SourceTree V2.0 입니다.\n");
-	printf("V1.0/Branch1 생성하였습니다.\n");
-	printf("V1.0/Branch2 생성하였습니다.\n");
-	printf("V1.0/Branch3 생성하였습니다.\n");
+	printf("모든 브랜치를 병합하였습니다.\n");
 }
